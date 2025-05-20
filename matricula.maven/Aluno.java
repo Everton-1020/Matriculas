e10ab36a74e89b82eab4bf7ee74bbd02192b5f48
Everton-1020 <vertonandrade2005@gmail.com>
2025-05-20 19:08:30 -0300

    Update Aluno.java

--- matricula.maven/Aluno.java
+++ matricula.maven/Aluno.java
@@ -1,22 +1,19 @@
-package matricula_maven;
+package matricula;
 
 public class Aluno {
-	 String nome;
+	  String nome;
 	    String matricula;
 	    String dataNascimento;
-	    String turma;
-	    
-	    public Aluno(String nome, String matricula, String dataNascimento, String turma) {
+
+	    public Aluno(String nome, String matricula, String dataNascimento) {
 	        this.nome = nome;
 	        this.matricula = matricula;
 	        this.dataNascimento = dataNascimento;
-	        this.turma = turma;
 	    }
-
 
 	    @Override
 	    public String toString() {
-	    	  return "Nome: " + nome + ", Matrícula: " + matricula + ", Data de Nascimento: " + dataNascimento + ", Turma: " + turma;
+	        return "Nome: " + nome + ", Matrícula: " + matricula + ", Data de Nascimento: " + dataNascimento;
 	    }
 	}
 
